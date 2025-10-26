02f1d0362a43f4a8868c410bf12ea8e9d80dd4c3
leolee9086 <464803712@qq.com>
2025-10-26 18:26:13 +0800

    改进代码组织

--- app/src/ai/chat.ts
+++ app/src/ai/chat.ts
@@ -1,27 +1,8 @@
 import { Dialog } from "../dialog";
 import { isMobile } from "../util/functions";
-import {  processAIChatRequestWithProtyle } from "./chat.confirm";
+import { processAIChatRequestWithProtyle } from "./chat.confirm";
 import { createVueComponentInDialog, VueComponentMountConfig } from "../util/vue/mount";
 import AiChatDialog from "../components/aiChatDialog.vue";
-
-// 处理取消事件
-const handleCancel = (dialog: Dialog) => {
-    dialog.destroy();
-};
-
-// 处理确认事件
-const handleConfirm = (
-    dialog: Dialog,
-    message: string,
-    protyle: IProtyle,
-    element: Element
-) => {
-    processAIChatRequestWithProtyle(
-        { dialog, element, protyle },
-        { msg: message }
-    );
-};
-
 // 创建聊天对话框Vue应用配置
 const createChatDialogVueConfig = (protyle: IProtyle, element: Element, dialog: Dialog): VueComponentMountConfig => {
     return {
@@ -29,8 +10,11 @@ const createChatDialogVueConfig = (protyle: IProtyle, element: Element, dialog: 
             AiChatDialog
         },
         eventHandlers: {
-            handleCancel: () => handleCancel(dialog),
-            handleConfirm: (message: string) => handleConfirm(dialog, message, protyle, element)
+            handleCancel: dialog.destroy,
+            handleConfirm: (message: string) => processAIChatRequestWithProtyle(
+                { dialog, element, protyle },
+                { msg: message }
+            )
         },
         template: `<AiChatDialog @cancel="handleCancel" @confirm="handleConfirm" ref="aiChatDialogComponent" />`,
         initMethodName: "focusChatInput"
