32aa9d436bb7cff5e192412bd9f702eb7e5978cb
leolee9086 <464803712@qq.com>
2025-11-01 11:22:43 +0800

    改进临时代码运行

--- app/src/util/code/scripts.excutor.ts
+++ app/src/util/code/scripts.excutor.ts
@@ -13,15 +13,12 @@
  * 创建临时ESM模块
  */
 export async function createTemporaryModule(code: string): Promise<TemporaryModule> {
-
     // 动态导入模块
     // 创建Blob URL
     const blob = new Blob([code], { type: 'application/javascript' });
     const moduleUrl = URL.createObjectURL(blob);
-
     try {
         const moduleExport = await import(/* webpackIgnore: true */ moduleUrl);
-
         // 返回模块信息和清理函数
         return {
             moduleUrl,
