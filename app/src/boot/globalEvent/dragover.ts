107be9a76a3a974d3df548dd680349dab30ea0cc
leolee9086 <464803712@qq.com>
2025-11-03 23:39:33 +0800

    强化类型校验,清理类型错误

--- app/src/boot/globalEvent/dragover.ts
+++ app/src/boot/globalEvent/dragover.ts
@@ -1,19 +1,19 @@
 import {Constants} from "../../constants";
-
 export const cancelDrag = () => {
     const ghostElement = document.getElementById("dragGhost");
     if (ghostElement) {
         if (ghostElement.dataset.ghostType === "dock") {
-            ghostElement.parentElement.querySelectorAll(".dock__item").forEach((item: HTMLElement) => {
+            ghostElement.parentElement?.querySelectorAll(".dock__item").forEach((item) => {
+                if(item instanceof HTMLElement)
                 item.style.opacity = "";
             });
             document.querySelector("#dockMoveItem")?.remove();
         } else {
-            const startElement = ghostElement.parentElement.querySelector(`[data-node-id="${ghostElement.getAttribute("data-node-id")}"]`) as HTMLElement;
-            if (startElement) {
+            const startElement = ghostElement.parentElement?.querySelector(`[data-node-id="${ghostElement.getAttribute("data-node-id")}"]`) ;
+            if (startElement instanceof HTMLElement) {
                 startElement.style.opacity = "";
             }
-            ghostElement.parentElement.querySelectorAll(".dragover__top, .dragover__bottom, .dragover, .dragover__current").forEach((item: HTMLElement) => {
+            ghostElement.parentElement?.querySelectorAll(".dragover__top, .dragover__bottom, .dragover, .dragover__current").forEach((item: HTMLElement) => {
                 item.classList.remove("dragover__top", "dragover__bottom", "dragover", "dragover__current");
                 item.style.opacity = "";
             });
