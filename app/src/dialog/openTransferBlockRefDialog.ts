5ea29b618d73c8f619e62fd8efae9a2c282cbc17
leolee9086 <464803712@qq.com>
2025-11-04 17:28:45 +0800

    拆离块引用转移弹窗

---
+++ app/src/dialog/openTransferBlockRefDialog.ts
@@ -0,0 +1,43 @@
+import { fetchPost } from "../ai/imports";
+import { Constants } from "../constants";
+import { Dialog } from ".";
+import { isMobile } from "../util/functions";
+import { siyuanI18n } from "../util/siyuanEnvironments/i18n.getI18n";
+
+export const openTransferBlockRefDialog = (id: string) => {
+    const renameDialog = new Dialog({
+        title: siyuanI18n.transferBlockRef,
+        content: `<div class="b3-dialog__content">
+    <input class="b3-text-field fn__block" placeholder="${siyuanI18n.targetBlockID}">
+    <div class="b3-label__text">${siyuanI18n.transferBlockRefTip}</div>
+</div>
+<div class="b3-dialog__action">
+    <button class="b3-button b3-button--cancel">${siyuanI18n.cancel}</button><div class="fn__space"></div>
+    <button class="b3-button b3-button--text">${siyuanI18n.confirm}</button>
+</div>`,
+        width: isMobile() ? "92vw" : "520px",
+    });
+    renameDialog.element.setAttribute("data-key", Constants.DIALOG_TRANSFERBLOCKREF);
+    const inputElement = renameDialog.element.querySelector("input");
+    const cancelElement = renameDialog.element.querySelector(".b3-button--cancel");
+    const textButtonElement = renameDialog.element.querySelector(".b3-button--text");
+
+    if (inputElement) {
+        renameDialog.bindInput(inputElement, () => {
+            if (textButtonElement instanceof HTMLButtonElement) {
+                textButtonElement.click();
+            }
+        });
+        inputElement.focus();
+        cancelElement && cancelElement.addEventListener("click", () => {
+            renameDialog.destroy();
+        });
+        textButtonElement && textButtonElement.addEventListener("click", () => {
+            fetchPost("/api/block/transferBlockRef", {
+                fromID: id,
+                toID: inputElement.value,
+            });
+            renameDialog.destroy();
+        });
+    }
+};
--- app/src/menus/block.ts
+++ app/src/menus/block.ts
@@ -1,46 +1,13 @@
 import { MenuItem } from "./Menu.Item";
-import { Dialog } from "../dialog";
-import { isMobile } from "../util/functions";
-import { fetchPost } from "../util/fetch";
-import { Constants } from "../constants";
+import { getGlobalMenus } from "../util/siyuanEnvironments/getMenu";
+import { siyuanI18n } from "../util/siyuanEnvironments/i18n.getI18n";
+import { openTransferBlockRefDialog } from "../dialog/openTransferBlockRefDialog";
 
 export const transferBlockRef = (id: string) => {
-    window.siyuan.menus.menu.append(new MenuItem({
+    getGlobalMenus().menu.append(new MenuItem({
         id: "transferBlockRef",
-        label: window.siyuan.languages.transferBlockRef,
+        label: siyuanI18n.transferBlockRef,
         icon: "iconScrollHoriz",
-        click() {
-            const renameDialog = new Dialog({
-                title: window.siyuan.languages.transferBlockRef,
-                content: `<div class="b3-dialog__content">
-    <input class="b3-text-field fn__block" placeholder="${window.siyuan.languages.targetBlockID}">
-    <div class="b3-label__text">${window.siyuan.languages.transferBlockRefTip}</div>
-</div>
-<div class="b3-dialog__action">
-    <button class="b3-button b3-button--cancel">${window.siyuan.languages.cancel}</button><div class="fn__space"></div>
-    <button class="b3-button b3-button--text">${window.siyuan.languages.confirm}</button>
-</div>`,
-                width: isMobile() ? "92vw" : "520px",
-            });
-            renameDialog.element.setAttribute("data-key", Constants.DIALOG_TRANSFERBLOCKREF);
-            const inputElement = renameDialog.element.querySelector("input");
-            const btnsElement = renameDialog.element.querySelectorAll(".b3-button");
-            renameDialog.bindInput(inputElement, () => {
-                if (btnsElement[1] instanceof HTMLButtonElement) {
-                    btnsElement[1].click();
-                }
-            });
-            inputElement.focus();
-            btnsElement[0].addEventListener("click", () => {
-                renameDialog.destroy();
-            });
-            btnsElement[1].addEventListener("click", () => {
-                fetchPost("/api/block/transferBlockRef", {
-                    fromID: id,
-                    toID: inputElement.value,
-                });
-                renameDialog.destroy();
-            });
-        }
+        click: () => openTransferBlockRefDialog(id)
     }).element);
 };
