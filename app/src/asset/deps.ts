0bb115210d65db593d5f341fa67fd9cf83f3aaa0
leolee9086 <464803712@qq.com>
2025-11-07 02:20:23 +0800

    修正文件名

--- app/src/asset/deps.ts
+++
@@ -1,8 +0,0 @@
-// Vue相关依赖
-export { createApp } from "vue";
-
-// 工具函数依赖
-export { createSimpleVueComponentLoader } from "../util/vue/mount";
-
-// 组件依赖
-export { default as ImageViewer } from "../components/imageEditor.vue";
