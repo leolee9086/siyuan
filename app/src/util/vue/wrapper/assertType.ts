d02c7f99207348ac28570ead06b1aba77bbb1077
leolee9086 <464803712@qq.com>
2025-11-17 12:53:37 +0800

    改进组件绑定

--- app/src/util/vue/wrapper/assertType.ts
+++ app/src/util/vue/wrapper/assertType.ts
@@ -2,8 +2,8 @@
  * 来自vue内部逻辑
  */
 
-export function assertType(value, type) {
-  let valid;
+export function assertType(value: unknown, type: any) {
+  let valid: boolean;
   const expectedType = getType(type);
   if (expectedType === "null") {
     valid = value === null;
@@ -14,9 +14,9 @@ export function assertType(value, type) {
       valid = value instanceof type;
     }
   } else if (expectedType === "Object") {
-    valid = shared.isObject(value);
+    valid = isObject(value);
   } else if (expectedType === "Array") {
-    valid = shared.isArray(value);
+    valid = isArray(value);
   } else {
     valid = value instanceof type;
   }
@@ -25,7 +25,8 @@ export function assertType(value, type) {
     expectedType
   };
 }
-export function getType(ctor) {
+
+export function getType(ctor: any): string {
   if (ctor === null) {
     return "null";
   }
@@ -37,24 +38,29 @@ export function getType(ctor) {
   }
   return "";
 }
-function makeMap(str) {
+
+function makeMap(str: string) {
   const map = /* @__PURE__ */ Object.create(null);
   for (const key of str.split(",")) map[key] = 1;
-  return (val) => val in map;
+  return (val: string) => val in map;
 }
+
 const isSimpleType = /* @__PURE__ */ makeMap(
   "String,Number,Boolean,Function,Symbol,BigInt"
 );
-const isMap = (val) => toTypeString(val) === "[object Map]";
-const isSet = (val) => toTypeString(val) === "[object Set]";
-const isDate = (val) => toTypeString(val) === "[object Date]";
-const isRegExp = (val) => toTypeString(val) === "[object RegExp]";
-const isFunction = (val) => typeof val === "function";
-const isString = (val) => typeof val === "string";
-const isSymbol = (val) => typeof val === "symbol";
-const isObject = (val) => val !== null && typeof val === "object";
-const isPromise = (val) => {
-  return (isObject(val) || isFunction(val)) && isFunction(val.then) && isFunction(val.catch);
+
+const isArray = Array.isArray;
+
+const isMap = (val: unknown) => toTypeString(val) === "[object Map]";
+const isSet = (val: unknown) => toTypeString(val) === "[object Set]";
+const isDate = (val: unknown) => toTypeString(val) === "[object Date]";
+const isRegExp = (val: unknown) => toTypeString(val) === "[object RegExp]";
+const isFunction = (val: unknown): val is Function => typeof val === "function";
+const isString = (val: unknown): val is string => typeof val === "string";
+const isSymbol = (val: unknown): val is symbol => typeof val === "symbol";
+const isObject = (val: unknown): val is Record<any, any> => val !== null && typeof val === "object";
+const isPromise = (val: unknown) => {
+  return (isObject(val) || isFunction(val)) && isFunction((val as any).then) && isFunction((val as any).catch);
 };
 const objectToString = Object.prototype.toString;
-const toTypeString = (value) => objectToString.call(value);
+const toTypeString = (value: unknown) => objectToString.call(value);
