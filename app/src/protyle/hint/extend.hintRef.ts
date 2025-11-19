0728a704581543ae8212c18878676f51278f18b7
leolee9086 <464803712@qq.com>
2025-11-19 23:40:50 +0800

    清理严格模式ts报错

--- app/src/protyle/hint/extend.hintRef.ts
+++ app/src/protyle/hint/extend.hintRef.ts
@@ -1,6 +1,7 @@
 import { Constants } from "../../constants";
 import { replaceFileName } from "../../editor/rename";
 import { fetchPost } from "../../util/fetch";
+import { siyuanI18n } from "../../util/siyuanEnvironments/i18n.getI18n";
 import { hasClosestBlock } from "../util/hasClosest";
 import { getEditorRange } from "../util/selection";
 import { genHintItemHTML } from "./extend";
@@ -10,7 +11,7 @@ const genNewFileItem = (k: string) => {
     return {
         value: `((newFile "${newFileName}"${Constants.ZWSP}'${newFileName}${Lute.Caret}'))`,
         html: `<div class="b3-list-item__first"><svg class="b3-list-item__graphic"><use xlink:href="#iconFile"></use></svg>
-<span class="b3-list-item__text">${window.siyuan.languages.newFile} <mark>${k}</mark></span></div>`,
+<span class="b3-list-item__text">${siyuanI18n.newFile} <mark>${k}</mark></span></div>`,
     }
 }
 
@@ -23,8 +24,8 @@ const genNewFileItem = (k: string) => {
  * @returns 块引用的HTML字符串
  */
 const genBlockRefValue = (item: IBlock, key: string, source: THintSource, nodeElement?: Element): string => {
-    const refText = item.name || item.refText.replace(new RegExp(Constants.ZWSP, "g"), "");
-    
+    const refText = item.name ||(item.refText?item.refText.replace(new RegExp(Constants.ZWSP, "g"), ""):"*");
+
     if (source === "search") {
         return `<span data-type="block-ref" data-id="${item.id}" data-subtype="s">${key}${Constants.ZWSP}${refText}</span>`;
     } else if (source === "av") {
@@ -38,7 +39,13 @@ const genBlockRefValue = (item: IBlock, key: string, source: THintSource, nodeEl
     }
 }
 export const hintRef = (key: string, protyle: IProtyle, source: THintSource): IHintData[] => {
+    if(!protyle.wysiwyg){
+        throw new Error("hintRef 方法调用时, protyle.wysiwyg 未定义");
+    }
     const nodeElement = hasClosestBlock(getEditorRange(protyle.wysiwyg.element).startContainer);
+    if (!protyle.hint) {
+        throw new Error("hintRef 方法调用时, protyle.hint 未定义");
+    }
     protyle.hint.genLoading(protyle);
     fetchPost("/api/search/searchRefBlock", {
         k: key,
@@ -48,6 +55,9 @@ export const hintRef = (key: string, protyle: IProtyle, source: THintSource): IH
         isDatabase: source === "av",
         isSquareBrackets: ["[[", "【【"].includes(protyle.hint.splitChar)
     }, (response) => {
+        if (!protyle.hint) {
+            throw new Error("hintRef 方法调用时, protyle.hint 未定义");
+        }
         const dataList: IHintData[] = [];
         if (response.data.newDoc) {
             dataList.push(genNewFileItem(response.data.k));
@@ -66,10 +76,10 @@ export const hintRef = (key: string, protyle: IProtyle, source: THintSource): IH
         if (dataList.length === 0) {
             dataList.push({
                 value: "",
-                html: window.siyuan.languages.emptyContent,
+                html: siyuanI18n.emptyContent,
             });
         } else if (response.data.newDoc && dataList.length > 1) {
-            dataList[1].focus = true;
+            dataList[1]&&(dataList[1].focus = true);
         }
         protyle.hint.genHTML(dataList, protyle, true, source);
     });
