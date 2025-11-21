ba78e95724876f7aea19691b71b970817b33e497
leolee9086 <464803712@qq.com>
2025-11-21 18:13:17 +0800

    清理严格模式ts报错

--- app/src/mobile/menu/model.ts
+++ app/src/mobile/menu/model.ts
@@ -5,16 +5,29 @@ export const openModel = (obj: {
     bindEvent: (element: HTMLElement) => void
 }) => {
     const modelElement = document.getElementById("model");
+    if(!modelElement) {
+        throw new Error("模型元素未找到");
+    }
     modelElement.style.transform = "translateY(0px)";
     modelElement.style.zIndex = (++window.siyuan.zIndex).toString();
     const iconElement  = modelElement.querySelector(".toolbar__icon");
+    if(!iconElement) {
+        throw new Error("图标元素未找到");
+    }
     if(obj.icon) {
         iconElement.classList.remove("fn__none");
-        iconElement.querySelector("use").setAttribute("xlink:href", "#" + obj.icon);
+        const useElement = iconElement.querySelector("use");
+        if(useElement){
+        useElement.setAttribute("xlink:href", "#" + obj.icon);
+        }
     } else {
         iconElement.classList.add("fn__none");
     }
-    modelElement.querySelector(".toolbar__text").innerHTML = obj.title;
+    const toolbartextElement = modelElement.querySelector(".toolbar__text");
+    if(!toolbartextElement) {
+        throw new Error("工具栏文本元素未找到");
+    }
+    toolbartextElement.innerHTML = obj.title;
     const modelMainElement = modelElement.querySelector("#modelMain") as HTMLElement;
     modelMainElement.innerHTML = obj.html;
     obj.bindEvent(modelMainElement);
