a28dec280031ad445f7e883ce4c561cd44c415bc
leolee9086 <464803712@qq.com>
2025-11-21 16:39:28 +0800

    拆分编辑器通用快捷键定义到单独文件

--- app/src/config/configSchemas/keymap.editor.general.ts
+++ app/src/config/configSchemas/keymap.editor.general.ts
@@ -1,172 +1,115 @@
 import z from "zod";
-import {schema as generalSchema} from "./keymap.editor.general";
-//键盘映射中的editor部分
-const schema = z.object({
-    general: generalSchema,
-    heading: z.object({
-        heading1: z.object({
+//键盘映射中的editor部分中的一般设置
+export const schema =z.object({
+        ai: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        heading2: z.object({
+        alignCenter: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        heading3: z.object({
+        alignLeft: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        heading4: z.object({
+        alignRight: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        heading5: z.object({
+        attr: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        heading6: z.object({
+        backlinks: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        paragraph: z.object({
-            custom: z.string(),
-            default: z.string()
-        })
-    }),
-    insert: z.object({
-        appearance: z.object({
+        collapse: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        bold: z.object({
+        copyBlockEmbed: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        check: z.object({
+        copyBlockRef: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        clearInline: z.object({
+        copyHPath: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        code: z.object({
+        copyID: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        "inline-code": z.object({
+        copyPlainText: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        "inline-math": z.object({
+        copyProtocol: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        italic: z.object({
+        copyProtocolInMd: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        kbd: z.object({
+        copyText: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        link: z.object({
+        duplicate: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        mark: z.object({
+        exitFocus: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        strike: z.object({
+        expand: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        sub: z.object({
+        expandDown: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        sup: z.object({
+        expandUp: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        tag: z.object({
+        fullscreen: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        underline: z.object({
+        graphView: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        lastUsed: z.object({
+        hLayout: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        memo: z.object({
+        insertAfter: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        ref: z.object({
+        insertBefore: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        table: z.object({
-            custom: z.string(),
-            default: z.string()
-        })
-    }),
-    list: z.object({
-        checkToggle: z.object({
+        insertBottom: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        indent: z.object({
+        insertRight: z.object({
             custom: z.string(),
             default: z.string()
         }),
-        outdent: z.object({
-            custom: z.string(),
-            default: z.string()
-        })
-    }),
-    table: z.object({
-        "delete-column": z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
-        "delete-row": z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
-        insertColumnLeft: z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
-        insertColumnRight: z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
-        insertRowAbove: z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
-        insertRowBelow: z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
-        "move-column-left": z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
-        "move-column-right": z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
-        "move-row-down": z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
-        "move-row-up": z.object({
+        jumpToParentNext: z.object({
             custom: z.string(),
             default: z.string()
         }),
@@ -174,22 +117,101 @@ const schema = z.object({
             custom: z.string(),
             default: z.string()
         }),
-        moveToLeft: z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
-        moveToRight: z.object({
-            custom: z.string(),
-            default: z.string()
-        }),
         moveToUp: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        netAssets2LocalAssets: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        netImg2LocalAsset: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        newContentFile: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        newNameFile: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        newNameSettingFile: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        openBy: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        optimizeTypography: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        outline: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        preview: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        quickMakeCard: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        redo: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        refPopover: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        refresh: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        refTab: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        rename: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        showInFolder: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        spaceRepetition: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        switchReadonly: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        switchAdjust: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        undo: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        vLayout: z.object({
+            custom: z.string(),
+            default: z.string()
+        }),
+        wysiwyg: z.object({
             custom: z.string(),
             default: z.string()
         })
     })
-})
-export {schema as editorKeyMapSchema}
-export const parseAsConfig = (rawConf: {}): Config.IConf['keymap']['editor'] => {
+export {schema as editorKeyMapGeneralSchema}
+export const parseAsConfig = (rawConf: {}): Config.IConf['keymap']['editor']['general'] => {
     const result = schema.safeParse(rawConf);
 
     if (!result.success) {
