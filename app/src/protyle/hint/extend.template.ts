2d09575b9596aff7337464218173d6d5e86118a3
leolee9086 <464803712@qq.com>
2025-11-23 04:54:12 +0800

    改进编码结构

---
+++ app/src/protyle/hint/extend.template.ts
@@ -0,0 +1,17 @@
+import { Constants } from "../../constants"
+import { siyuanI18n } from "../../util/siyuanEnvironments/i18n.getI18n"
+
+export const templateItem = (ctx: { key: string, protyle: IProtyle }) => {
+    return {
+        filter: [siyuanI18n.template, "template", "模板", "moban", "muban", "mb"],
+        id: "template",
+        value: Constants.ZWSP,
+        html: /*html*/`
+        <div class="b3-list-item__first">
+            <svg class="b3-list-item__graphic">
+                <use xlink:href="#iconMarkdown"></use>
+            </svg>
+            <span class="b3-list-item__text">${siyuanI18n.template}</span>
+        </div>`,
+    }
+}
---
+++ app/src/protyle/hint/extend.widget.ts
@@ -0,0 +1,13 @@
+import { Constants } from "../../constants"
+import { siyuanI18n } from "../../util/siyuanEnvironments/i18n.getI18n"
+
+export const widgetItem = (ctx: { key: string, protyle: IProtyle }) => {
+    return {
+        filter: [siyuanI18n.widget, "widget", "挂件", "guajian", "gj"],
+        id: "widget",
+        value: Constants.ZWSP + 1,
+        html: /*html*/`<div class="b3-list-item__first"><svg class="b3-list-item__graphic">
+        <use xlink:href="#iconBoth"></use>
+    </svg><span class="b3-list-item__text">${siyuanI18n.widget}</span></div>`,
+    }
+}
