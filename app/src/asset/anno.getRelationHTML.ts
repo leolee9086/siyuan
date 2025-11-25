f9044f98c7401d570add9271e63ffad7709cb93f
leolee9086 <464803712@qq.com>
2025-11-26 00:39:37 +0800

    清理严格模式ts报错

--- app/src/asset/anno.getRelationHTML.ts
+++ app/src/asset/anno.getRelationHTML.ts
@@ -1,12 +1,14 @@
+import { siyuanI18n } from "../util/siyuanEnvironments/i18n.getI18n";
+
 export const getRelationHTML = (ids: string[]) => {
     if (!ids) {
-        return `<li class="b3-list--empty">${window.siyuan.languages.emptyContent}</li>`;
+        return `<li class="b3-list--empty">${siyuanI18n.emptyContent}</li>`;
     }
     let html = "";
     ids.forEach((id: string) => {
         html += `<li data-id="${id}" class="popover__block b3-list-item b3-list-item--narrow b3-list-item--hide-action">
     <span class="b3-list-item__text">${id}</span>
-    <span data-type="clear" class="b3-tooltips b3-tooltips__w b3-list-item__action" aria-label="${window.siyuan.languages.delete}">
+    <span data-type="clear" class="b3-tooltips b3-tooltips__w b3-list-item__action" aria-label="${siyuanI18n.delete}">
         <svg><use xlink:href="#iconTrashcan"></use></svg>
     </span>
 </li>`;
