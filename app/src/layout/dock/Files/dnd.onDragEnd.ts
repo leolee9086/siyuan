954677d97dfc47bd8ff341d858419f1cfe10bd51
leolee9086 <464803712@qq.com>
2025-12-18 18:56:31 +0800

    改进代码组织

---
+++ app/src/layout/dock/Files/dnd.onDragEnd.ts
@@ -0,0 +1,43 @@
+import { Files } from "../Files";
+import { Constants } from "../../../constants";
+import { showTooltip } from "../../../dialog/tooltip";
+import { hasClosestByClassName } from "../../../protyle/util/hasClosest";
+import { setSiyuanDragElement } from "../../../util/siyuanEnvironments/getSiyuanConfig.environment";
+/// #if !BROWSER
+import { ipcRenderer } from "electron";
+/// #endif
+
+export const onDragEnd = (files: Files, event: DragEvent) => {
+    files.parent.panelElement.classList.remove("sy__file--disablehover");
+    const opacityItems = files.element.querySelectorAll('.b3-list-item[style*="opacity: 0.38;"]');
+    for (const [index, item] of Array.from(opacityItems).entries()) {
+        if (!(item instanceof HTMLElement)) {
+            continue;
+        }
+        item.style.opacity = "";
+        // https://github.com/siyuan-note/siyuan/issues/11587
+        if (index !== 0) {
+            continue;
+        }
+
+        const targetElement = document.elementFromPoint(event.clientX, event.clientY);
+        if (!targetElement || !hasClosestByClassName(targetElement, "sy__file")) {
+            continue;
+        }
+
+        const ariaLabelElement = item.querySelector(".ariaLabel");
+        const ariaLabel = ariaLabelElement?.getAttribute("aria-label");
+        if (ariaLabelElement && ariaLabel) {
+            showTooltip(ariaLabel, ariaLabelElement);
+        }
+    }
+    setSiyuanDragElement(undefined);
+    /// #if !BROWSER
+    ipcRenderer.send(Constants.SIYUAN_SEND_WINDOWS, { cmd: "resetTabsStyle", data: "rmDragStyle" });
+    /// #else
+    const dragTabs = document.querySelectorAll(".layout-tab-bars--drag");
+    for (const item of Array.from(dragTabs)) {
+        item.classList.remove("layout-tab-bars--drag");
+    }
+    /// #endif
+};
--- app/src/layout/dock/Files/dnd.ts
+++ app/src/layout/dock/Files/dnd.ts
@@ -1,60 +1,38 @@
 
 import { Files } from "../Files";
-import { Constants } from "../../../constants";
-import { showTooltip } from "../../../dialog/tooltip";
-import {
-    hasClosestByClassName
-} from "../../../protyle/util/hasClosest";
-/// #if !BROWSER
-import { ipcRenderer } from "electron";
-/// #endif
+
 import { onDragStart } from "./dnd.onDragStart";
 import { onDragOver } from "./dnd.onDragOver";
 import { onDrop } from "./dnd.onDrop";
+import { onDragEnd } from "./dnd.onDragEnd";
 
 export const initFilesDrag = (files: Files) => {
-    files.element.addEventListener("dragstart", (event: DragEvent & { target: HTMLElement }) => {
-        onDragStart(files, event);
+    files.element.addEventListener("dragstart", (event: DragEvent) => {
+        onDragStart(files, event as DragEvent & { target: HTMLElement });
     });
     files.element.addEventListener("dragend", (event) => {
-        files.parent.panelElement.classList.remove("sy__file--disablehover");
-        files.element.querySelectorAll('.b3-list-item[style*="opacity: 0.38;"]').forEach((item: HTMLElement, index) => {
-            item.style.opacity = "";
-            // https://github.com/siyuan-note/siyuan/issues/11587
-            if (index === 0 && hasClosestByClassName(document.elementFromPoint(event.clientX, event.clientY), "sy__file")) {
-                const ariaLabelElement = item.querySelector(".ariaLabel");
-                if (ariaLabelElement) {
-                    showTooltip(ariaLabelElement.getAttribute("aria-label"), ariaLabelElement);
-                }
-            }
-        });
-        window.siyuan.dragElement = undefined;
-        /// #if !BROWSER
-        ipcRenderer.send(Constants.SIYUAN_SEND_WINDOWS, { cmd: "resetTabsStyle", data: "rmDragStyle" });
-        /// #else
-        document.querySelectorAll(".layout-tab-bars--drag").forEach(item => {
-            item.classList.remove("layout-tab-bars--drag");
-        });
-        /// #endif
+        onDragEnd(files, event);
     });
-    files.element.addEventListener("dragover", (event: DragEvent & { target: HTMLElement }) => {
+    files.element.addEventListener("dragover", (event: any) => {
         onDragOver(files, event);
     });
     let counter = 0;
+    // @内联回调
     files.element.addEventListener("dragleave", () => {
         counter--;
         if (counter === 0) {
-            files.element.querySelectorAll(".dragover, .dragover__bottom, .dragover__top").forEach((item: HTMLElement) => {
+            const dragOverElements = files.element.querySelectorAll(".dragover, .dragover__bottom, .dragover__top");
+            for (const item of Array.from(dragOverElements)) {
                 item.classList.remove("dragover", "dragover__bottom", "dragover__top");
-            });
+            }
         }
     });
     files.element.addEventListener("dragenter", (event) => {
         event.preventDefault();
         counter++;
     });
-    files.element.addEventListener("drop", async (event: DragEvent & { target: HTMLElement }) => {
+    files.element.addEventListener("drop", async (event: DragEvent) => {
         counter = 0;
-        await onDrop(files, event);
+        await onDrop(files, event as DragEvent & { target: HTMLElement });
     });
 };
