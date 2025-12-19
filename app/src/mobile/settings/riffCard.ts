d3a88ff36653c2a277ba2e4092de96e1d9197cc8
leolee9086 <464803712@qq.com>
2025-12-19 16:14:04 +0800

    保存riffCard.ts的当前修改

--- app/src/mobile/settings/riffCard.ts
+++ app/src/mobile/settings/riffCard.ts
@@ -1,9 +1,10 @@
 import {openModel} from "../menu/model";
 import {flashcard} from "../../config/flashcard";
+import {siyuanI18n} from "../../util/siyuanEnvironments/i18n.getI18n.environment";
 
 export const initRiffCard = () => {
     openModel({
-        title: window.siyuan.languages.riffCard,
+        title: siyuanI18n.riffCard,
         icon: "iconRiffCard",
         html: flashcard.genHTML(),
         bindEvent(modelMainElement: HTMLElement) {
