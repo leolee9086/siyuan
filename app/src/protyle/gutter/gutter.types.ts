6c43007428bfa48328818e11d461b061abea877c
leolee9086 <464803712@qq.com>
2025-12-20 01:27:18 +0800

    创建类型文件,修正lint错误

---
+++ app/src/protyle/gutter/gutter.types.ts
@@ -0,0 +1,12 @@
+/**
+ * Gutter 模块类型定义
+ * 包含 gutter 相关的接口和类型定义
+ */
+
+/**
+ * Gutter 编辑菜单上下文接口
+ */
+export interface IGutterEditMenuContext {
+    protyle: IProtyle;
+    nodeElement: Element;
+}
