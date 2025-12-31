8780dd42019285ccddd8058e506e216f29383f6d
leolee9086 <464803712@qq.com>
2025-12-31 17:30:48 +0800

    改进代码组织,添加守卫函数

--- app/src/util/DOM/element.guard.ts
+++ app/src/util/DOM/element.guard.ts
@@ -21,3 +21,8 @@ export const isHTMLSelectElement = (el: unknown): el is HTMLSelectElement => {
 export const isCustomEvent = <T = unknown>(event: Event): event is CustomEvent<T> => {
     return event instanceof CustomEvent;
 };
+
+/** 类型守卫：判断节点是否为 SVGElement */
+export const isSVGElement = (node: unknown): node is SVGElement => {
+    return node instanceof SVGElement;
+};
